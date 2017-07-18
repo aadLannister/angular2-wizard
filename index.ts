18f947a28c47da14c76dce49fa0c38f44ddaa784
aadLannister <ibrahim.aadlani@gmail.com>
2017-07-18 17:30:44 +0200

    Update index.ts
    Ajout du module de traduction

--- index.ts
+++ index.ts
@@ -6,9 +6,19 @@ import { WizardStepComponent } from './src/wizard-step.component';
 export * from './src/wizard.component';
 export * from './src/wizard-step.component';
 
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [Http]
+      }
+    }),
   ],
   declarations: [
     WizardComponent,
